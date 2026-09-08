4d15e01e1b0247dbfc89d7e8a13cf37f05a26fed
tejaskumarkoneti <git@stackblitz.com>
2026-09-09 01:05:07 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 100x-cohort-dev
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/tejaskumarkoneti/100x-cohort-dev)
---
+++ Week 1/Excercise2.js
@@ -0,0 +1,21 @@
+///Write a program with three variables, each with the following value:
+
+///The first variable contains your name
+
+///The second variable contains your age
+
+///The third variable contains your occupation
+
+///Then print the variables using the console.log() method. Here's the example output
+
+const name = 'Tejas';
+
+let age = 23;
+
+var occuption = 'developer';
+
+console.log(name)
+
+console.log(age)
+
+console.log(occuption)
